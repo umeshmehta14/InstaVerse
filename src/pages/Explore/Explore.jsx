3304c694b0ce33f14161c769e1d171377e664c24
umeshmehta14 <ishaanmehta782@gmail.com>
2023-06-16 17:53:46 +0530

    Refactor: explore page posts

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -5,25 +5,31 @@ import { Flex, VStack } from "@chakra-ui/react";
 import { usePost } from "../../contexts";
 import { useEffect } from "react";
 import { heroContentBox } from "../../styles/GlobalStyles";
+import { postFilter } from "../../utils/PostFilter";
+import { SET_FILTER } from "../../utils/Constants";
 
 export const Explore = () => {
   const {
-    postState: { posts },
+    postDispatch,
+    postState: { posts, filter },
   } = usePost();
 
+  const ExplorePosts = postFilter(posts, filter);
+
   useEffect(() => {
+    postDispatch({ type: SET_FILTER, payload: "latest" });
     window.scrollTo({ top: 0 });
   }, []);
 
   return (
-    <Flex {...heroContentBox}>
+    <Flex sx={heroContentBox}>
       <UserSuggestion />
       <VStack
         w={{ base: "100%", lg: "auto" }}
         alignItems={"center"}
         minW={{ md: "468px" }}
       >
-        {posts.map((post) => (
+        {ExplorePosts.map((post) => (
           <PostBox key={post._id} post={post} />
         ))}
       </VStack>
