b991dce7bab28956c75996227fb0dfb8238af839
umeshmehta14 <ishaanmehta782@gmail.com>
2023-06-16 15:22:09 +0530

    Add: unfollow user confirmation modal

---
+++ src/components/Post Box/PostBox Components/UnfollowModal.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const UnfollowModal = ({ isOpen, onClose }) => {
+  return (
+    <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Modal Title</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text fontWeight="bold" mb="1rem">
+            You can scroll the content behind the modal
+          </Text>
+          <Lorem count={2} />
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            Close
+          </Button>
+          <Button variant="ghost">Secondary Action</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default UnfollowModal;
