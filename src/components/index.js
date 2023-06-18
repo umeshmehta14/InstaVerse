700c601416f1d1a26c2bb632ada5a9fceadbb84d
umeshmehta14 <ishaanmehta782@gmail.com>
2023-06-18 23:30:37 +0530

    fix: naming convention bug

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,4 @@ export { PrivateRoute } from "./RequireAuth/PrivateRoute.jsx";
 export { PostModal } from "./Post Modal1/PostModal.jsx";
 export { UserListModal } from "./UserListModal/UserListModal.jsx";
 export { FollowedByUsers } from "./FollowedByUsers/FollowedByUsers.jsx";
-export { NavBar } from "./Navbar/NavBar.jsx";
+export { NavBar } from "./Navbar/NavBar1.jsx";
